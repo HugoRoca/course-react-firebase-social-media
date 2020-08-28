4ed8f7ff94b10723ecdca263e66576c8cc5e8e36
Hugo Roca <hugo.rock20@hotmail.com>
2020-08-27 22:42:01 -0500

    get user

--- functions/functions/index.js
+++ functions/functions/index.js
@@ -1,6 +1,6 @@
 const functions = require("firebase-functions");
 const { getAllScreams, postOneScream } = require("./handlers/screams");
-const { signup, login, uploadImage, addUserDetails } = require("./handlers/users");
+const { signup, login, uploadImage, addUserDetails, getAuthenticateUser } = require("./handlers/users");
 const FBAuth = require('./utils/fbAuth')
 const app = require("express")();
 
@@ -9,6 +9,7 @@ app.get("/screams", getAllScreams);
 app.post("/scream", FBAuth, postOneScream);
 app.post('/user/image', FBAuth, uploadImage)
 app.post('/user', FBAuth, addUserDetails)
+app.get('/user', FBAuth, getAuthenticateUser)
 
 // Users
 app.post("/signup", signup);
