4c7eb5fe752232f774dbeaaa6bc852248cb33665
Hugo Roca <hugo.rock20@hotmail.com>
2020-08-27 22:12:59 -0500

    update and upload image

--- functions/functions/index.js
+++ functions/functions/index.js
@@ -1,6 +1,6 @@
 const functions = require("firebase-functions");
 const { getAllScreams, postOneScream } = require("./handlers/screams");
-const { signup, login } = require("./handlers/users");
+const { signup, login, uploadImage } = require("./handlers/users");
 const FBAuth = require('./utils/fbAuth')
 const app = require("express")();
 
@@ -11,5 +11,6 @@ app.post("/scream", FBAuth, postOneScream);
 // Users
 app.post("/signup", signup);
 app.post("/login", login);
+app.post('/user/image', FBAuth, uploadImage)
 
 exports.api = functions.https.onRequest(app);
