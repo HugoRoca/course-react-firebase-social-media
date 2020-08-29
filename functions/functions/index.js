c47a693f8d91db1e267aa4587665ee573b0f1dc8
Hugo Roca <hugo.rock20@hotmail.com>
2020-08-28 23:25:26 -0500

    endpoint for get screams and comments

--- functions/functions/index.js
+++ functions/functions/index.js
@@ -1,18 +1,26 @@
 const functions = require("firebase-functions");
 const { getAllScreams, postOneScream } = require("./handlers/screams");
-const { signup, login, uploadImage, addUserDetails, getAuthenticateUser } = require("./handlers/users");
-const FBAuth = require('./utils/fbAuth')
+const {
+  signup,
+  login,
+  uploadImage,
+  addUserDetails,
+  getAuthenticateUser,
+  getScream,
+} = require("./handlers/users");
+const FBAuth = require("./utils/fbAuth");
 const app = require("express")();
 
 // Screams
 app.get("/screams", getAllScreams);
 app.post("/scream", FBAuth, postOneScream);
-app.post('/user/image', FBAuth, uploadImage)
-app.post('/user', FBAuth, addUserDetails)
-app.get('/user', FBAuth, getAuthenticateUser)
+app.get("/scream/:screamId", getScream);
 
 // Users
 app.post("/signup", signup);
 app.post("/login", login);
+app.post("/user/image", FBAuth, uploadImage);
+app.post("/user", FBAuth, addUserDetails);
+app.get("/user", FBAuth, getAuthenticateUser);
 
 exports.api = functions.https.onRequest(app);
