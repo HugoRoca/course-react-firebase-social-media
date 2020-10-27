03409d346f6a06e3f20ce73588ce09b9b127adc7
Hugo Roca <hugo.rock20@hotmail.com>
2020-10-26 23:00:23 -0500

    add new theme

--- client/src/App.js
+++ client/src/App.js
@@ -5,21 +5,42 @@ import login from "./pages/login";
 import signup from "./pages/signup";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import CreateMuiTheme from "@material-ui/core/styles/createMuiTheme";
+
+const theme = CreateMuiTheme({
+  palette: {
+    primary: {
+      light: "#33c9dc",
+      main: "#00bcd4",
+      dark: "#008394",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#ff6333",
+      main: "#ff3d99",
+      dark: "#b22a00",
+      contrastText: "#fff",
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Navbar />
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component={home} />
-            <Route exact path="/login" component={login} />
-            <Route exact path="/signup" component={signup} />
-          </Switch>
-        </div>
-      </Router>
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <div className="App">
+        <Router>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={home} />
+              <Route exact path="/login" component={login} />
+              <Route exact path="/signup" component={signup} />
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    </MuiThemeProvider>
   );
 }
 
