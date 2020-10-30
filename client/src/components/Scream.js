f7b3147600553a4d0b0bda4c6678a008c177a92c
Hugo Roca <hugo.rock20@hotmail.com>
2020-10-29 23:16:28 -0500

    add styles cover

--- client/src/components/Scream.js
+++ client/src/components/Scream.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import withStyles from "@material-ui/core/styles/withStyles";
-import Link from 'react-router-dom/Link'
+import Link from "react-router-dom/Link";
 // MUI stuff
 import Card from "@material-ui/core/Card";
 import CardMedia from "@material-ui/core/CardMedia";
@@ -10,6 +10,14 @@ import Typography from "@material-ui/core/Typography";
 const styles = {
   card: {
     display: "flex",
+    marginBottom: 20,
+  },
+  image: {
+    minWidth: 200,
+  },
+  content: {
+    padding: 25,
+    objectFit: "cover",
   },
 };
 
@@ -29,11 +37,24 @@ class Scream extends Component {
     } = this.props;
 
     return (
-      <Card>
-        <CardMedia image={userImage} title="Profile image" />
-        <CardContent>
-          <Typography variant="h5" component={Link} to={`/users/${userHandle}`} color="primary">{userHandle}</Typography>
-          <Typography variant="body2" color="textSecondary">{createdAt}</Typography>
+      <Card className={classes.card}>
+        <CardMedia
+          image={userImage}
+          title="Profile image"
+          className={classes.image}
+        />
+        <CardContent class={classes.content}>
+          <Typography
+            variant="h5"
+            component={Link}
+            to={`/users/${userHandle}`}
+            color="primary"
+          >
+            {userHandle}
+          </Typography>
+          <Typography variant="body2" color="textSecondary">
+            {createdAt}
+          </Typography>
           <Typography variant="body1">{body}</Typography>
         </CardContent>
       </Card>
