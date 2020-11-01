866f177af8c6d6f601f381096d455af1e5ae8d1d
Hugo Roca <hugo.rock20@hotmail.com>
2020-10-31 23:48:34 -0500

    implement grid

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,11 +1,33 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import withStyles from "@material-ui/core/styles/withStyles";
+import PropTypes from 'prop-types'
+// MUI stuff
+import Grid from '@material-ui/core/Grid'
 
-export default class login extends Component {
+const styles = {
+  form: {
+    textAlign: 'center'
+  }
+};
+
+class login extends Component {
   render() {
+    const { classes } = this.props
+
     return (
-      <div>
-        <h1>login Page</h1>
-      </div>
-    )
+      <Grid container className={classes.form}>
+        <Grid item sm />
+        <Grid item sm>
+          <p>Yo</p>
+        </Grid>
+        <Grid item sm />
+      </Grid>
+    );
   }
 }
+
+login.propTypes = {
+  classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(login);
