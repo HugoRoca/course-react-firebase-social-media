05abcbab0c50cdc3f526b9128b78ee88968b5b9e
Hugo Roca <hugo.rock20@hotmail.com>
2020-10-31 23:40:49 -0500

    use dayjs for time

--- client/src/components/Scream.js
+++ client/src/components/Scream.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import withStyles from "@material-ui/core/styles/withStyles";
 import { Link } from "react-router-dom";
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
 // MUI stuff
 import Card from "@material-ui/core/Card";
 import CardMedia from "@material-ui/core/CardMedia";
@@ -23,6 +25,8 @@ const styles = {
 
 class Scream extends Component {
   render() {
+    dayjs.extend(relativeTime)
+
     const {
       classes,
       scream: {
@@ -53,7 +57,7 @@ class Scream extends Component {
             {userHandle}
           </Typography>
           <Typography variant="body2" color="textSecondary">
-            {createdAt}
+            {dayjs(createdAt).fromNow()}
           </Typography>
           <Typography variant="body1">{body}</Typography>
         </CardContent>
