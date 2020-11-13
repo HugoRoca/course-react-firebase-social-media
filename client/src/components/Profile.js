24dc009f84d7ee4315eb5f8f8a74f47657843219
Hugo Roca <hugo.rock20@hotmail.com>
2020-11-12 22:54:30 -0500

    add icons and config new component profile

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,13 +1,40 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+// MUI stuff
+import withStyles from "@material-ui/core/styles/withStyles";
+import Button from "@material-ui/core/Button";
+import Paper from "@material-ui/core/Paper";
+// Icons
+
+const styles = (theme) => ({
+  ...theme.spreadThis,
+});
 
 class Profile extends Component {
   render() {
-    return (
-      <div>
-        
-      </div>
-    )
+    const {
+      classes,
+      user: {
+        credentials: { handle, createdAt, imageUrl, bio, website, location },
+        loading,
+      },
+    } = this.props;
+    
+    let profileMarkup = !loading ? (authenticated ? (
+      <Paper
+    ):()) : (<p>loading...</p>);
+
+    return profileMarkup
   }
 }
+const mapStateToProps = (state) => ({
+  user: state.user,
+});
 
-export default Profile
+Profile.propTypes = {
+  user: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired,
+};
+
+export default connect(mapStateToProps)(withStyles(styles)(Profile));
--- client/src/redux/reducers/userReducer.js
+++ client/src/redux/reducers/userReducer.js
@@ -1,7 +1,8 @@
 import {
   SET_USER,
   SET_AUTHENTICATED,
-  SET_UNAUTHENTICATED
+  SET_UNAUTHENTICATED,
+  LOADING_USER,
 } from "../types";
 
 const initialState = {
@@ -9,6 +10,7 @@ const initialState = {
   credentials: {},
   likes: [],
   notifications: [],
+  loading: false,
 };
 
 export default function (state = initialState, action) {
@@ -23,7 +25,13 @@ export default function (state = initialState, action) {
     case SET_USER:
       return {
         authenticated: true,
+        loading: false,
         ...action.payload,
+      };
+    case LOADING_USER:
+      return {
+        ...state,
+        loading: true,
       };
     default:
       return state;
